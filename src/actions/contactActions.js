a446bf8b69bb5b87d29e8aca50ec2a196bb37d66
Abayomi Aishida <yomicorperate@gmail.com>
2020-04-12 12:31:09 +0100

    made an async request to jsonplaceholder through axios to fill state with contacts

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,9 +1,12 @@
 import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from "../actions/types";
+import axios from "axios";
 
-export const getContacts = () => {
-  return {
+export const getContacts = () => async (dispatch) => {
+  const res = await axios.get("http://jsonplaceholder.typicode.com/users");
+  dispatch({
     type: GET_CONTACTS,
-  };
+    payload: res.data,
+  });
 };
 
 export const deleteContact = (id) => {
